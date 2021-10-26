52ea67b5c85ac85df39c718fab05ac6b8397592c
Stephen <stepan.kopylov@ibitcy.com>
2021-10-27 00:21:15 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import {AiFillLinkedin, AiFillGithub, AiFillInstagram, AiOutlineMail} from 'react-icons/ai';
-
 import Lottie from "lottie-react";
-
-import groovyWalkAnimation from "./static/data.json";
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+import animation from "./static/data.json";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAy0-BgvHaOiUQd2xXKqi-TGJ1YNoNnVjA",
@@ -26,20 +24,20 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Lottie style={{width: 300, height: 100, marginBottom: 0}} animationData={groovyWalkAnimation}/>
-                <p style={{fontSize: 24, marginTop: 40, marginBottom: 40}}>
+                <Lottie className={"Lottie"} animationData={animation}/>
+                <p className="Main-p">
                     Hi, I'm Stephen!
                 </p>
-                <p style={{fontSize: 24, marginBottom: 0, marginTop: 0}}>
-                    <a style={{marginLeft: 5, marginRight: 5}} href={'http://github.com/stephenkopylov'}
+                <p className="Link-p">
+                    <a className="Link-a" href={'http://github.com/stephenkopylov'}
                        target={'_blank'}><AiFillGithub/></a>
-                    <a style={{marginLeft: 5, marginRight: 5}}
+                    <a className="Link-a"
                        href={'https://www.linkedin.com/in/stephen-kopylov/'}
                        target={'_blank'}><AiFillLinkedin/></a>
-                    <a style={{marginLeft: 5, marginRight: 5}}
+                    <a className="Link-a"
                        href={'mailto:newonxp@gmail.com'}
                        target={'_blank'}><AiOutlineMail/></a>
-                    <a style={{marginLeft: 5, marginRight: 5}}
+                    <a className="Link-a"
                        href={'https://www.instagram.com/newon/'}
                        target={'_blank'}><AiFillInstagram/></a>
                 </p>
